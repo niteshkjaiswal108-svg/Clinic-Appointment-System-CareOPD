652e1162dd4685cb8f5f4f71bd91f6b3352d5714
niteshkjaiswal108 <niteshkjaiswal108@gmail.com>
2026-01-08 19:30:53 +0530

    trigger redeploy

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -15,7 +15,7 @@ const AppContextProvider = (props) => {
 
     const getDoctorsData = async () => {
         try {
-            const { data } = await axios.get(backendUrl + '/api/doctor/list')
+            const { data }  = await axios.get(backendUrl + '/api/doctor/list')
             if (data.success) {
                 setDoctors(data.doctors)
             } else {
